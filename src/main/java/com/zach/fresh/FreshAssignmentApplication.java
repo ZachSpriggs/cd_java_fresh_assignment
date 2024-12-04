2ba69075a5eb1d075bc51ab3ab227352ae0d2301
Zach Spriggs <zachspriggs@gmail.com>
2024-12-03 18:34:39 -0800

    pushing FreshAssignment for java stack at coding dojo

---
+++ src/main/java/com/zach/fresh/FreshAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.zach.fresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/zach/fresh/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.zach.fresh;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FreshAssignmentApplication.class);
+	}
+
+}
